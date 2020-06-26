9005742b078d31bc08bc5184b85dcf16201458d6
plaetzaw <plaetzaw@gmail.com>
2020-06-25 19:04:01 -0500

    added React-Router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+//===Pages===
+import Login from "./components/login";
+import Register from "./components/registration";
+import Create from "./components/createTask";
+import Feed from "./components/taskFeed";
 //===Routing===
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/createtask" component={Create} />
+      <Route exact path="/taskfeed" component={Feed} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
