ff545e905acab886ed9088e12f04f2c63b626b72
plaetzaw <plaetzaw@gmail.com>
2020-06-26 22:40:32 -0500

    push

--- src/components/login.js
+++ src/components/login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export class login extends Component {
   render() {
-    return <div>LOGIN PAGE</div>;
+    return <div>LOGIN PAGE GOES HERE</div>;
   }
 }
 
